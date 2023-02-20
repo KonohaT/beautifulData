5141592a9fd9ddb513c110508dc5c41e8333c724
nicksit12 <117672206+nicksit12@users.noreply.github.com>
2023-02-20 16:42:24 -0700

    Add files via upload

---
+++ Project/src/handwritten/NeuralNetwork.py
@@ -0,0 +1,59 @@
+import numpy
+import scipy
+import csv
+
+
+class NeuralNetwork:
+
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        self.lr = learningrate
+
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+
+        self.activation_function = lambda x: scipy.special.expit(x)
+
+    def train(self, inputs_list, targets_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        output_errors = targets - final_outputs
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)),
+                                        numpy.transpose(hidden_outputs))
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+
+                                        numpy.transpose(inputs))
+
+    def save_weights(self, epoch):
+        with open(f'weights_epoch_{epoch}.csv', mode='w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Weight', 'Input Node', 'Hidden Node', 'Output Node', 'Layer Index'])
+            for i, wih_row in enumerate(self.wih):
+                for j, wih in enumerate(wih_row):
+                    writer.writerow([wih, j, i, -1, 1])
+            for i, who_row in enumerate(self.who):
+                for j, who in enumerate(who_row):
+                    writer.writerow([who, -1, j, i, 2])
+
+    def query(self, inputs_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
